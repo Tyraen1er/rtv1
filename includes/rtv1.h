3fa66474c764f148c2688bd7554088572b475c1b
Emmanuel FERRANDINI <eferrand@e3r8p12.42.fr>
2017-06-29 17:33:48 +0200

    maj .h

--- includes/rtv1.h
+++ includes/rtv1.h
@@ -6,12 +6,18 @@
 /*   By: bmoiroud <bmoiroud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/23 14:54:15 by bmoiroud          #+#    #+#             */
-/*   Updated: 2017/06/27 19:16:22 by bmoiroud         ###   ########.fr       */
+/*   Updated: 2017/06/29 15:01:47 by bmoiroud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RTV1_H
 # define RTV1_H
+
+# define WIN_W 1200
+# define WIN_H 800
+# define IMG_W WIN_W
+# define IMG_H WIN_H
+# define COLOR 16711680
 
 # include <math.h>
 # include <stdio.h>
@@ -24,7 +30,16 @@ typedef struct	s_coord
 	double		x;
 	double		y;
 	double		z;
+	int			set;
 }				t_coord;
+
+typedef struct	s_size
+{
+	double		height;
+	double		width;
+	double		depth;
+	int			set;
+}				t_size;
 
 typedef struct	s_ray
 {
@@ -38,20 +53,26 @@ typedef struct	s_ray
 	double		e2;
 }				t_ray;
 
+typedef struct	s_light
+{
+	int			intensity;
+	t_coord		pos;
+}				t_light;
+
 typedef struct	s_rotation
 {
 	double		rot_x;
 	double		rot_y;
 	double		rot_z;
+	int			set
 }				t_rotation;
 
 typedef struct	s_object
 {
 	int			color;
 	int			type;
-	double		height;
-	double		width;
-	double		depth;
+	int			brightness;
+	t_size		size;
 	t_coord		pos;
 	t_rotation	rotation;
 }				t_object;
@@ -65,6 +86,7 @@ typedef struct	s_eye
 typedef struct	s_rtv1
 {
 	t_object	*objects;
+	t_light		*lights;
 	t_eye		eye;
 }				t_rtv1;
 
@@ -73,6 +95,8 @@ typedef struct	s_win
 	int			bpp;
 	int			endian;
 	int			linelen;
+	int			width;
+	int			height;
 	char		*data;
 	void		*img;
 	void		*mlx;
@@ -85,8 +109,18 @@ typedef struct	s_data
 	t_rtv1		rtv1;
 }				t_data;
 
+void	ft_parse(char *file, t_data *data);
 void	ft_cast_ray(t_ray *r, t_coord s, t_coord dst);
 void	ft_init_ray(t_ray *ray, t_coord *a, t_coord b);
+void	ft_init_win(t_data *data);
+void	ft_get_dimension(t_object *obj, int fd);
+void	ft_get_color(int *color, int fd);
+void	ft_get_brightness(int *brightness, int fd);
+void	ft_init_ray(t_ray *ray, t_coord *a, t_coord b);
 void	ft_init_data(t_data *data);
+void	ft_init_objects(char *file, t_rtv1 *rtv1);
+void	ft_error(int error);
+int		ft_objectid(char *str);
+int		ft_check_numbers(char *str);
 
 #endif
--- srcs/ft_collision.c
+++ srcs/ft_collision.c
@@ -31,29 +31,25 @@ double	*rotate(double *vector, t_rotation deg)
 	double			*tmp;
 	double			*rot;
 
-	rot = (double[11]){3, 3, 1, 0, 0, 0, cos(deg.rot_x), -sin(deg.rot_x), 0,
-		sin(deg.rot_x), cos(deg.rot_x)};
+	rot = (double[11]){3, 3, 1, 0, 0, 0, cos(deg.rot_x), -sin(deg.rot_x),
+		0, sin(deg.rot_x), cos(deg.rot_x)};
 	ret = matrice_multi_double(rot, vector);
-	printf("premier transformation\n%lf %lf\n%lf %lf %lf\n", ret[0], ret[1], ret[2], ret[3], ret[4]);
 	tmp = ret;
 	rot = (double[11]){3, 3, cos(deg.rot_y), 0, sin(deg.rot_y), 0, 1, 0,
 		-sin(deg.rot_y), 0, cos(deg.rot_y)};
 	ret = matrice_multi_double(rot, tmp);
-	printf("\ndeuxieme transformation\n%lf %lf\n%lf %lf %lf\n", ret[0], ret[1], ret[2], ret[3], ret[4]);
 	ft_memdel((void*)&tmp);
 	tmp = ret;
 	rot = (double[11]){3, 3, cos(deg.rot_z), -sin(deg.rot_z), 0, sin(deg.rot_z),
 		cos(deg.rot_z), 0, 0, 0, 1};
 	ret = matrice_multi_double(rot, tmp);
-	printf("\nfinal:\n%lf %lf\n%lf %lf %lf\n", ret[0], ret[1], ret[2], ret[3], ret[4]);
 	ft_memdel((void*)&tmp);
 	return (ret);
 }
 
-int		ft_col_sphere(t_object obj, t_coord pos)
+int		ft_col_sphere(t_object obj, t_coord point)
 {
-	if (obj.height <= sqrt(pow((obj.pos.x - pos.x), 2) +
-				pow((obj.pos.y - pos.y), 2) + pow((obj.pos.z - pos.z), 2)))
+	if (sqrt(pow(obj.pos.x - point.x, 2) + pow(obj.pos.y - point.y, 2) + pow(obj.pos.z - point.z, 2)) <= obj.size.height)
 		return (1);
 	return (0);
 }
@@ -62,32 +58,102 @@ int		ft_col_plan(t_object obj, t_coord point)
 {
 	double		*corner_a;
 	double		*corner_b;
-
+	double		const;
 	t_rotation	rot;
 
-	corner_a = (double[5]){1, 3, obj.width / 2, obj.height / 2, obj.depth / 2};
+	corner_a = (double[5]){1, 3, obj.size.width / 2, obj.size.height / 2, obj.size.depth / 2};
 	corner_a = rotate(corner_a, obj.rotation);
-	corner_a = (double[5]){1, 3, corner_a[2] + obj.pos.x, corner_a[3] +
-		obj.pos.y, corner_a[4] + obj.pos.z};
-	corner_b = (double[5]){1, 3, -obj.width / 2, obj.height / 2, obj.depth / 2};
+	corner_b = (double[5]){1, 3, -obj.size.width / 2, obj.size.height / 2, obj.size.depth / 2};
 	corner_b = rotate(corner_b, obj.rotation);
 	corner_b = (double[5]){1, 3, corner_b[2] + obj.pos.x, corner_b[3] +
 		obj.pos.y, corner_b[4] + obj.pos.z};
-
+	const = corner_a[2] * corner_b[2] + corner_a[3] * corner_b[3] +
+		corner_a[4] * corner_b[4];
+	if (const == corner_a[2] * point.x + corner_a[3] * point.y +
+			corner_a[4] * point.z)
+		return (1);
 	return (0);
 }
 
-int		ft_col_cone(t_object obj, t_coord pos)
+int		ft_col_cone(t_object obj, t_coord point)
 {
+	double	*center;
+	double	*top;
+	double	coef_cone;
+	double	coef_pt;
+
+	center = (double[5]){1, 3, -obj.size.height / 2, 0};
+	top = (double[5]){1, 3, 0, obj.size.height / 2, 0};
+	center = rotate(center, obj.rotation);
+	top = rotate(top, obj.rotation);
+	center = (double[5]){1, 3, center[2] + obj.pos.x, center[3] + obj.pos.y,
+		center[4] + obj.pos.z};
+	top = (double[5]){1, 3, top[2] + obj.pos.x, top[3] + obj.pos.y,
+		top[4] + obj.pos.z};
+	if (!(center[3] <= point.y && point.y <= top[3]) &&
+			!(top[3] <= point.y && point.y <= center[3]))
+		return (0);
+	coef_cone = fabs(height / width);
+	coef_pt = fabs(top[3] - point.y * (obj.size.width / (top[3] - center[3]));
+	if (coef_pt <= coef_cone))
+		return (1);
 	return (0);
 }
 
-int		ft_col_cylinder(t_object obj, t_coord pos)
+int		ft_col_cylinder(t_object obj, t_coord point)
 {
+	double	*center;
+	double	*top;
+	double	*perp;
+	double	ratio;
+
+	center = (double[5]){1, 3, -obj.size.height / 2, 0};
+	top = (double[5]){1, 3, 0, obj.size.height / 2, 0};
+	center = rotate(center, obj.rotation);
+	top = rotate(top, obj.rotation);
+	center = (double[5]){1, 3, center[2] + obj.pos.x, center[3] +
+		obj.pos.y, center[4] + obj.pos.z};
+	top = (double[5]){1, 3, top[2] + obj.pos.x, top[3] + obj.pos.y,
+		top[4] + obj.pos.z};
+	if (!(center[3] <= point.y && point.y <= top[3]) ||
+			!(top[3] <= point.y && point.y <= center[3]))
+		return (0);
+	ratio = (point.y - center[3]) / (center[3] - obj.pos.y);
+	perp = (double[5]){1, 3, center[2] + ratio * (center[2] - obj.pos.x), point.y, center[4] + ratio * (center[4] - obj.pos.z)};
+	if (sqrt(pow(perp[2] - point.x ,2) + pow(perp[3] - point.y ,2) +
+				pow(perp[4] - point.z ,2)) <= width)
+		return (1);
 	return (0);
 }
 
 int main()
 {
+	t_object	cone;
+	t_object	cylinder;
+	t_object	plan;
+	t_object	sphere;
+	t_coord		point;
+
+	cone.height = 10;
+	cone.width = 2;
+	cone.pos = (t_coord){5, 12, 8};
+	cone.rotation = (t_rotation){0, 0, 0};
+	point = (t_coord){3, 7, 8};
+
+	cylinder.height = 10;
+	cylinder.width = 2;
+	cylinder.pos = (t_coord){5, 12, 8};
+	cylinder.rotation = (t_rotation){0, 0, 0};
+	point = (t_coord){};
+
+	plan.height = ;
+	plan.width = ;
+	plan.depth = ;
+	plan.pos = (t_coord){};
+	plan.rotation = (t_rotation){};
+	point = (t_coord){};
+
+	sphere.height = ;
+	sphere.pos = ;
 	return (0);
 }
